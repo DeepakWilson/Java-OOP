d205c97c3bde7f1dc7aca257f5f72f52d0b3c775
Deepak Wilson <deepakwilson2018@gmail.com>
2024-10-03 11:54:33 +0530

    Add files via upload

---
+++ createfile.java
@@ -0,0 +1,18 @@
+package javalab;
+import java.io.File;
+import java.io.IOException;
+public class createfile {
+	public static void main(String[] args) {
+	try {
+		File myObj= new File("1.txt");
+		if(myObj.createNewFile()) {
+			System.out.println("File created:"+myObj.getName());
+		}else {
+			System.out.println("File exists already");
+		}
+	}catch(IOException e) {
+		System.out.println("An error occured- ");
+		e.printStackTrace();
+	}
+	}
+}
